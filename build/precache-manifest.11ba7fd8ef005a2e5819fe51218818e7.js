fc9e8a9ed1f2e6c797f9fc4f6ad0aed67fc499e5
Matt Argo <mattdude1081@gmail.com>
2020-08-09 12:50:42 -0700

    Done for now

---
+++ build/precache-manifest.11ba7fd8ef005a2e5819fe51218818e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69db837e6b2784ae0c2b171e2223cc5e",
+    "url": "/hackster-news/index.html"
+  },
+  {
+    "revision": "d0155b6f9bcb312dd530",
+    "url": "/hackster-news/static/css/main.c663e9a1.chunk.css"
+  },
+  {
+    "revision": "addc55ce767320bbf212",
+    "url": "/hackster-news/static/js/2.f350292b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackster-news/static/js/2.f350292b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0155b6f9bcb312dd530",
+    "url": "/hackster-news/static/js/main.ceda016f.chunk.js"
+  },
+  {
+    "revision": "a309c4a4bdde3920363a",
+    "url": "/hackster-news/static/js/runtime-main.7dda3151.js"
+  },
+  {
+    "revision": "24fcb7e10053bce15c0b3b709ff52515",
+    "url": "/hackster-news/static/media/DINPro-Medium.24fcb7e1.woff2"
+  },
+  {
+    "revision": "393be0a3ca68cc15d76a588c85eb3f3a",
+    "url": "/hackster-news/static/media/DINPro-Medium.393be0a3.woff"
+  },
+  {
+    "revision": "ebe4c0835b5b7dbc8223e991c5338793",
+    "url": "/hackster-news/static/media/ProximaNova-Bold.ebe4c083.woff2"
+  },
+  {
+    "revision": "f813564f4bfd1f82d896cc1f079d3942",
+    "url": "/hackster-news/static/media/ProximaNova-Bold.f813564f.woff"
+  },
+  {
+    "revision": "33d769f54d6c2b903198b1bbbcda6ecf",
+    "url": "/hackster-news/static/media/ProximaNova-Regular.33d769f5.woff"
+  },
+  {
+    "revision": "40faf4973c93ae633491e55db8cbe625",
+    "url": "/hackster-news/static/media/ProximaNova-Regular.40faf497.woff2"
+  }
+]);
