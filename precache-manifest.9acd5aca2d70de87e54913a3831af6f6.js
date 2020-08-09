f33318e43be9b5ed6584b7bcdb123342e0963783
Matt Argo <mattdude1081@gmail.com>
2020-08-09 12:23:51 -0700

    Updates

---
+++ precache-manifest.9acd5aca2d70de87e54913a3831af6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8935b3a7d1203523572157f2f0077e2f",
+    "url": "/hackster-news/index.html"
+  },
+  {
+    "revision": "e8c3881e0e2da4d11fc5",
+    "url": "/hackster-news/static/css/main.c4cf9d87.chunk.css"
+  },
+  {
+    "revision": "addc55ce767320bbf212",
+    "url": "/hackster-news/static/js/2.f350292b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackster-news/static/js/2.f350292b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c3881e0e2da4d11fc5",
+    "url": "/hackster-news/static/js/main.8eb8958b.chunk.js"
+  },
+  {
+    "revision": "a309c4a4bdde3920363a",
+    "url": "/hackster-news/static/js/runtime-main.7dda3151.js"
+  }
+]);
